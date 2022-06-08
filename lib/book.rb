668cb97623c0fdf67a95b6e52934543ccbf62844
Kevin Nielsen <knielsen24@gmail.com>
2022-06-07 19:59:04 -0700

    commit tree cc2a5ab0404c2a12f8335451166395c99129ecd0

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,33 @@
+require 'pry'
+
+class Book
+	attr_accessor :title, :author, :page_count, :genre
+
+	def initialize(title)
+		@title = title
+	end
+
+	def properties(author, page_count, genre)
+		@author = author
+		@page_count = page_count
+		@genre = genre
+	end
+
+	def turn_page
+		puts "Flipping the page...wow, you read fast!"
+	end
+
+end
+
+and_then_there_were_none = Book.new("And Then There Were None")
+
+and_then_there_were_none.properties("Agatha Christie", 272, "Mystery")
+
+# and_then_there_were_none.author = "Agatha Christie"
+# and_then_there_were_none.page_count = 272
+# and_then_there_were_none.genre = "Mystery"
+
+
+binding.pry
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,27 @@
-# Make your shoe class here!
+require 'pry'
+
+class Shoe
+	attr_accessor :brand, :color, :size, :material, :condition
+
+	def initialize(brand)
+		@brand = brand
+	end
+
+	def properties(color, size, material, condition)
+		@color = color
+		@size = size
+		@material = material
+		@condition = condition
+	end
+
+	def cobble
+		puts "Your shoe is as good as new!"
+		@condition = "new"
+	end
+
+end
+
+nike = Shoe.new("Nike")
+nike.properties("red", "10", "suede", "tattered")
+
+binding.pry
